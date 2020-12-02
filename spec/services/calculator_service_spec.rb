2f720795ad546c013fc73b89c5f3b4d183c31992
Brady Clark <bradyjclark88@gmail.com>
2020-12-03 04:13:47 +1100

    this should be enough specs for now

--- spec/services/calculator_service_spec.rb
+++ spec/services/calculator_service_spec.rb
@@ -2,27 +2,39 @@ require_relative '../../services/calculator_service'
 
 RSpec.describe CalculatorService do
 
+  let(:bundle1) { Bundle.new(3, 595) }
+  let(:bundle2) { Bundle.new(5, 995) }
+  let(:bundle3) { Bundle.new(9, 1699) }
+  let(:bundles) { [bundle1, bundle2, bundle3] }
+
   subject { described_class.new }
-  context 'can calculate' do
+
+  context 'can calculate, quantity bigger than largest bundle' do
     it 'returns the expected output' do
-      expect(subject.calculate(13, packs)).to eq 2585
+      expect(subject.calculate(13, bundles)).to eq 2585
+    end
+  end
+
+  context 'can calculate, quantity smaller than largest bundle' do
+    it 'returns the expected output' do
+      expect(subject.calculate(10, bundles)).to eq 1990
     end
   end
 
   context 'zero quantity' do
     it 'returns zero' do
-      expect(subject.calculate(-1, packs)).to eq 0
+      expect(subject.calculate(-1, bundles)).to eq 0
     end
   end
   context 'negative number' do
     it 'returns nil' do
-      expect(subject.calculate(-1, packs)).to eq nil
+      expect(subject.calculate(-1, bundles)).to eq nil
     end
   end
 
   context 'cannot calculate' do
     it 'returns nil' do
-      expect(subject.calculate(7, packs)).to eq nil
+      expect(subject.calculate(7, bundles)).to eq nil
     end
   end
 end
