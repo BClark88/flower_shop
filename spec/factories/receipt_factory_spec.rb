152501120e0ac58e3a0979cac625951285c4f830
Brady Clark <bradyjclark88@gmail.com>
2020-12-03 14:31:04 +1100

    Now we need to be able to print a receipt

--- spec/factories/receipt_factory_spec.rb
+++ spec/factories/receipt_factory_spec.rb
@@ -2,7 +2,7 @@ require_relative '../../factories/receipt_factory'
 
 RSpec.describe ReceiptFactory do
   describe '#print_receipt' do
-    subject { described_class.new(code, price_list, quantities).print_receipt }
+    subject { described_class.new(product, bundles) }
 
     describe 'print_receipt' do
       let(:product) { Product.new('my_product', 'mp', []) }
@@ -11,11 +11,11 @@ RSpec.describe ReceiptFactory do
       let(:bundles) { [bundle1, bundle2, bundle2] }
       let(:expected_output) do
         "13 mp $21.85\n" \
-        " 2 x 3 $5.95\n"
+        " 2 x 3 $5.95\n" \
         ' 1 x 5 $9.95'
       end
       it 'returns the expected output' do
-        expect(subject).to eq expected_output
+        expect(subject.print_receipt).to eq expected_output
       end
     end
   end
