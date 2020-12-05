37e65eb39e5d5c56cc157a71bdff8b8f2014cb19
Brady Clark <bradyjclark88@gmail.com>
2020-12-06 10:01:12 +1100

    Add receipt factory

--- spec/factories/receipt_factory_spec.rb
+++ spec/factories/receipt_factory_spec.rb
@@ -2,15 +2,15 @@ require_relative '../../factories/receipt_factory'
 
 RSpec.describe ReceiptFactory do
   describe '#print_receipt' do
-    subject { described_class.new(product, bundles) }
+    subject { described_class.new(product, packaged_bundles) }
 
     describe 'print_receipt' do
       let(:product) { Product.new('my_product', 'mp', []) }
       let(:bundle1) { Bundle.new(3, 595) }
       let(:bundle2) { Bundle.new(5, 995) }
-      let(:bundles) { [bundle1, bundle2, bundle2] }
+      let(:packaged_bundles) { [bundle1, bundle2, bundle1] }
       let(:expected_output) do
-        "13 mp $21.85\n" \
+        "11 mp $21.85\n" \
         " 2 x 3 $5.95\n" \
         ' 1 x 5 $9.95'
       end
